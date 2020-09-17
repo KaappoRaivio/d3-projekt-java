3156da3a0d1d1cbd5abd9974afe18800766cb756
Kaappo Raivio <kaappo.raivio@gmail.com>
2020-09-18 00:02:54 +0300

    initial commit

--- src/main/java/process/Process.java
+++
@@ -1,8 +0,0 @@
-package process;
-
-import entity.Entity;
-import java.util.List;
-
-public interface Process {
-    List<Entity> update(List<Entity> entities, double deltaTime, int frameCounter);
-}
